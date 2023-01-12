6ae53ef1dfad9d4dec9afccd625ba6a607aabac9
Ponti-D <spaediaz@gmail.com>
2023-01-12 15:04:18 +0000

    console and imports work

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,28 @@
+import { print, promptForUserInput } from './output.js';
 import { add } from "./calc.js";
 
-function startAdventure(name: string): void {
-	 console.log(add(5,2));
+print(`-----------------------------------------`);
+print(`-------------Welcome to Mars-------------`);
+print(`-----------------------------------------`);
+
+promptForUserInput(`Would you like to navigate a Rover to help with our mission? [Y/N]`,startAdventure)
+// export function showWelcomeMessage(){
+// 	userInput.question('Is this example useful? [y/n] ', (answer) => {
+// 		  switch(answer.toLowerCase()) {
+// 			case 'y':
+// 			  console.log('Super!');
+// 			  break;
+// 			case 'n':
+// 			  console.log('Sorry! :(');
+// 			  break;
+// 			default:
+// 			  console.log('Invalid answer!');
+// 		  }
+// 		  userInput.close();
+// 		});
+//   }
+
+function startAdventure(answer: string): void {
+	console.log(add(5,2));
+	print(`${answer}`);
 }
-startAdventure("");
---
+++ src/output.ts
@@ -0,0 +1,18 @@
+import * as readline from "node:readline";
+
+let userInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+export function promptForUserInput(
+  question: string,
+  callback: (answer: string) => void
+) {
+  userInput.question(`${question}`, callback);
+}
+
+export function print(str: string): void {
+  console.log(str);
+  console.log();
+}
